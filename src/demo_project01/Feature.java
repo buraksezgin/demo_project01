066a2f84845fe4b5f13cb53ab4dd6ebf343f60f6
buraksezgin <73567330+buraksezgin@users.noreply.github.com>
2020-10-30 02:04:10 -0500

    Update Feature.java
    I have committed first changes from the remote master branch

--- src/demo_project01/Feature.java
+++ src/demo_project01/Feature.java
@@ -4,6 +4,8 @@ public class Feature {
 
 	public static void main(String[] args) {
 		System.out.println("this is feature class");
+		System.out.println("this is the first commit from remote master branch");
+		
 
 	}
 
