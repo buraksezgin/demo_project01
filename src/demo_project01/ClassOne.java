4fab862248460b07a1c869942527bc717e05e5db
buraksezgin <brkszgn54@gmail.com>
2020-10-30 00:30:38 -0500

    this is conflict msg from master branch

--- src/demo_project01/ClassOne.java
+++ src/demo_project01/ClassOne.java
@@ -4,6 +4,7 @@ public class ClassOne {
 
 	public static void main(String[] args) {
 		System.out.println("This is just a demo");
+		System.out.println("this is where the conflict starts");
 	}
 
 }
